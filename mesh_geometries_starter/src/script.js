5ac4d8ce78f435baa9666f0924120dd6d253ee64
Patrick Abrena <patrickabrena@hotmail.com>
2025-02-14 00:32:46 -0600

    be in the middle of buffergeometry lesson

--- mesh_geometries_starter/src/script.js
+++ mesh_geometries_starter/src/script.js
@@ -5,9 +5,31 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 const scene = new THREE.Scene();
 
 // add objects to the scene
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+//const cubeGeometry = new THREE.BoxGeometry(1, 1, 1); // commenting this out so that we can create a new "geometry" variable that we can pass to the cubeMesh class later on
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+// CREATE CUSTOM GEOMETRY (2D TRIANGLE)
+//using buffer geometry to create custom geometry in three.js
+// buffer geometry stores the vertices as an array of binary numbers to map out coordinates
+// can use BufferAttribute to store information about position of the vertices so we can tell Three.js to set the position attribute using the information that we provide it
+
+//delcare "vertices" variable and instantiate new float32Array class which is a special array type in javascript that stores floating-point number with 32 bit precision
+// more efficient that a normal javascript array because it stores numbers a raw binary data
+// important for WebGl performance since the GPU requires data in this format for efficient rendering
+const vertices = new Float32Array([
+  0, 0, 0,
+  0, 2, 0,
+  2, 0, 0
+])
+
+const BufferAttribute = new THREE.BufferAttribute(vertices, 3)
+
+const geometry = new THREE.BufferGeometry();
+geometry.setAttribute('position', BufferAttribute)
+
+
+const cubeMesh = new THREE.Mesh(geometry, cubeMaterial)
+//const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
 scene.add(cubeMesh);
 
